518484e8c2132ab79f38d15384231afabcbedb96
Waldirio M Pinheiro <wpinheir@iroman.home>
2018-05-26 06:15:40 -0700

    const.py modified

--- const.py
+++ const.py
@@ -1,11 +1,12 @@
+import socket
 
 SAT_SERVER="http://sat631.local.domain"
 USERNAME="admin"
 PASSWORD="redhat"
-# CAPSULE_NAME=socket.gethostname()
 
-
-CAPSULE_NAME="sat631caps.local.domain"
+CAPSULE_NAME=socket.gethostname()
+# CAPSULE_NAME="sat631caps.local.domain"
+# test
 
 CAP_USERNAME="admin"
 CAP_PASSWORD="aZCehD5szBULC4z2EZhHFZGg5BJav9je"
